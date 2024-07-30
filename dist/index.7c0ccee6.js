ba32a30e7e6858e7d8265d31d0ca7dd365fe2bfd
sunny <saivamshidasari1432@gmail.com>
2024-07-30 02:28:14 -0400

    fourth commit

--- dist/index.7c0ccee6.js
+++
@@ -1,8 +0,0 @@
-const heading = React.createElement("h1", {
-    id: "heading"
-}, "hello world from react");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-let b = 5;
-root.render(heading);
-
-//# sourceMappingURL=index.7c0ccee6.js.map
