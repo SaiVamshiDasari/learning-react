faace2f44e861c00c4eb9bd1f3496bc04c273c64
sunny <saivamshidasari1432@gmail.com>
2024-08-01 18:49:35 -0400

    6th commit

--- src/app.js
+++ src/app.js
@@ -1,68 +1,66 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import Search from "./components/Search"
+import Search from "./components/Search";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import Resmenu from "./components/Resmenu";
 import Cart from "./components/Cart";
-import { createBrowserRouter, RouterProvider,Outlet } from "react-router-dom";
-import { Provider } from "react-redux";
-import appStore from "./utils/appStore";
 import Collections from "./components/Collections";
-import { BrowserRouter as Router } from 'react-router-dom';
 
-//import HorizontalScroll from "./components/HorizantalScroll";
-
-const App = () =>{
-    return(
-      <Router basename="/learning-react">
-      <Provider store={appStore}>
-        <div className="app">
-            <Header />
-            <Outlet />
-        </div>
-      </Provider></Router>
-    );
+const App = () => {
+  return (
+    <Provider store={appStore}>
+      <div className="app">
+        <Header />
+        <Outlet />
+      </div>
+    </Provider>
+  );
 };
 
-const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element:  <App /> ,
-    children:[
-      {
-        path:"/",
-        element:<Body />
-      },
-      {
-        path:"/about",
-        element: <About />,
-      },
-      {
-        path:"/contact",
-        element:<Contact />,
-      },
-      {
-        path:"/cart",
-        element:<Cart />,
-      },
-      {
-        path:"/restaurants/:resId",
-        element:<Resmenu />,
-      },
-      {
-        path:"/collections/:collectionName",
-        element: <Collections />,
-      },
-    ],
-    errorElement: <Error />
-    
-  },
- 
-])
+const appRouter = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "/",
+          element: <Body />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+        },
+        {
+          path: "/cart",
+          element: <Cart />,
+        },
+        {
+          path: "/restaurants/:resId",
+          element: <Resmenu />,
+        },
+        {
+          path: "/collections/:collectionName",
+          element: <Collections />,
+        },
+      ],
+      errorElement: <Error />,
+    },
+  ],
+  { basename: "/learning-react" } // Add this line
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={appRouter} />);
+
